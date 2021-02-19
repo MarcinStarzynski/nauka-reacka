21bd5c8699eb8c0fd421684db4c5cb4fc7484141
Marcin Starzynski <gienekmsms@gmail.com>
2021-02-19 14:17:49 +0100

    Add Columns component

---
+++ src/components/Column/Column.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles from './Column.scss';
+import PropTypes from 'prop-types';
+
+class Column extends React.Component {
+    static propTypes ={
+        titleColumn: PropTypes.node.isRequired,
+    }
+    static defaultProps ={
+        titleColumn: 'Insert title of column here',
+    }
+
+    render(){
+        return(
+            <section className={styles.component}>
+                <h3 className={styles.title}>{this.props.titleColumn}</h3>
+            </section>
+        )        
+    }
+}
+
+export default Column;
